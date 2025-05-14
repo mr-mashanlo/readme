bf226cbc824cfaff4e12ae38aac53f754bdc794f
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-05-14 17:30:12 +0500

    change fonts

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ import newer from 'gulp-newer';
 import postcss from 'gulp-postcss';
 import rename from 'gulp-rename';
 import gulpSass from 'gulp-sass';
-import ttf2woff2 from 'gulp-ttf2woff2';
+// import ttf2woff2 from 'gulp-ttf2woff2';
 import webp from 'gulp-webp';
 import { rollup } from 'rollup';
 import * as dartSass from 'sass';
@@ -89,7 +89,7 @@ function images() {
 function fonts() {
   return src( paths.fonts.src, { encoding: false } )
     .pipe( newer( { dest: paths.fonts.dest, ext: '.woff2' } ) )
-    .pipe( gulpIf( file => [ '.ttf' ].includes( file.extname ), ttf2woff2() ) )
+    // .pipe( gulpIf( file => [ '.ttf' ].includes( file.extname ), ttf2woff2() ) )
     .pipe( dest( paths.fonts.dest ) )
 }
 
@@ -110,11 +110,8 @@ function serve() {
   watch( paths.images.watch, images );
 }
 
-const build = series( clean, fonts, images, parallel( html, styles, scripts ) );
+export const build = series( clean, fonts, images, html, styles, scripts, serve );
 
-const dev = series( build, serve );
+export const dev = series( clean, fonts, images, parallel( html, styles, scripts ), serve );
 
-export {
-  dev as default,
-  build
-};
+export default dev;
